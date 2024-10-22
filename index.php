78bbbe88325c51446a3049949ca4eec304c0677b
SachBell <k_zack004@hotmail.com>
2024-10-22 13:51:33 -0500

    style: :lipstick: Update Style and Form Style

--- index.php
+++ index.php
@@ -46,18 +46,17 @@
         <form action="/src/config/App/proccess_form.php" method="POST">
             <div class="input-container container d-flex flex-column gap-4">
                 <div class="container-fluid d-flex justify-content-center">
-                    <h1>Selección de Prácticas Pre-Profecionales</h1>
+                    <h1>Selección de Prácticas Preprofecionales</h1>
                 </div>
                 <div class="container">
                     <p id="mensaje" style="display: <?= $mensaje === '' ? 'none' : 'block' ?>;">
                         <?= htmlspecialchars($mensaje) ?>
                     </p>
                     <script>
-                        window.onload = function() {
+                        window.onload = function () {
                             const mensajeElement = document.getElementById('mensaje');
                             if (mensajeElement.style.display === 'block') {
-                                setTimeout(function() {
-                                    window.location.href = '/';
+                                setTimeout(function () {
                                     mensajeElement.style.display = 'none';
                                 }, 5000);
                             }
@@ -65,75 +64,119 @@
                     </script>
                 </div>
                 <div class="container">
-                    <div class="row d-flex align-items-center">
-                        <div class="col-6">
-                            <!-- Nombres del Estudiante -->
-                            <legend class="container">
-                                <label for="names_input" class="form-label">Nombres</label>
-                                <input class="form-control" type="text" name="names_input" placeholder="Nombres"
-                                    required>
-                            </legend>
-                            <!-- Apellidos -->
-                            <legend class="container">
-                                <label class="form-label" for="lastnames_input">Apellidos</label>
-                                <input class="form-control" type="text" name="lastnames_input" placeholder="Apellidos"
-                                    required>
-                            </legend>
-                            <!-- C.I -->
-                            <legend class="container">
-                                <label class="form-label" for="ci_input">Cédula</label>
-                                <input class="form-control" type="text" name="ci_input"
-                                    placeholder="Cédula de Identidad" required>
-                            </legend>
-                            <!-- Correo Electrónico -->
-                            <legend class="container">
-                                <label class="form-label" for="email_input">Correo</label>
-                                <input class="form-control" type="email" name="email_input" placeholder="Correo"
-                                    required>
-                            </legend>
-                            <!-- Número de Teléfono -->
-                            <legend class="container">
-                                <label class="form-label" for="celNumber_input">Número Celular</label>
-                                <input class="form-control" type="text" name="celNumber_input" placeholder="Celular"
-                                    required>
-                            </legend>
+                    <div class="d-flex flex-column align-items-center gap-5">
+                        <div class="container">
+                            <fieldset>
+                                <legend class="text-center">Datos Personales</legend>
+                                <div class="container d-flex flex-column gap-4">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <!-- C.I -->
+                                            <div class="container mt-4">
+                                                <label class="form-label fs-5" for="ci_input">Cédula</label>
+                                                <input class="form-control" type="text" name="ci_input"
+                                                    placeholder="Cédula de Identidad" required>
+                                            </div>
+                                            <!-- Nombres del Estudiante -->
+                                            <div class="container mt-2">
+                                                <label for="names_input" class="form-label fs-5">Nombres</label>
+                                                <input class="form-control" type="text" name="names_input"
+                                                    placeholder="Nombres" required>
+                                            </div>
+                                            <!-- Apellidos del Estudiante -->
+                                            <div class="container mt-2">
+                                                <label class="form-label fs-5" for="lastnames_input">Apellidos</label>
+                                                <input class="form-control" type="text" name="lastnames_input"
+                                                    placeholder="Apellidos" required>
+                                            </div>
+                                            <!-- Número de Teléfono -->
+                                            <div class="container mt-2">
+                                                <label class="form-label fs-5" for="celNumber_input">Número
+                                                    Celular</label>
+                                                <input class="form-control" type="text" name="celNumber_input"
+                                                    placeholder="Celular" required>
+                                            </div>
+                                            <!-- Correo Electrónico -->
+                                            <div class="container mt-2">
+                                                <label class="form-label fs-5" for="email_input">Correo</label>
+                                                <input class="form-control" type="email" name="email_input"
+                                                    placeholder="Correo" required>
+                                            </div>
+                                            <!-- Dirección de Domicilio -->
+                                            <div class="container mt-2">
+                                                <label class="form-label fs-5" for="addressCity_input">Domicilio</label>
+                                                <input class="form-control" type="text" name="addressCity_input"
+                                                    placeholder="Dirección Domiciliaria" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <!-- Barrio -->
+                                            <div class="container mt-4">
+                                                <label class="form-label fs-5" for="neighborhood_input">Barrio</label>
+                                                <input class="form-control" type="text" name="neighborhood_input"
+                                                    placeholder="Barrio" required>
+                                            </div>
+                                            <!-- Sector -->
+                                            <div class="container mt-2">
+                                                <label class="form-label fs-5" for="quadrant_input">Sector</label>
+                                                <input class="form-control" type="text" name="quadrant_input"
+                                                    placeholder="Sector" required>
+                                            </div>
+                                            <div class="container mt-2">
+                                                <!-- Semestre -->
+                                                <div class="mt-2">
+                                                    <label class="form-label fs-5" for="semester_input">Semestre</label>
+                                                    <select class="form-select" name="semester_input" id="semester">
+                                                        <option selected disabled>Elige tu Semestre</option>
+                                                        <option value="Segundo">Segundo</option>
+                                                        <option value="Tercero">Tercero</option>
+                                                        <option value="Cuarto">Cuarto</option>
+                                                        <option value="Quinto">Quinto</option>
+                                                    </select>
+                                                </div>
+                                                <!-- Paralelo -->
+                                                <div class="pt-2">
+                                                    <label class="form-label fs-5" for="grade_input">Paralelo</label>
+                                                    <select class="form-select" name="grade_input" id="grade">
+                                                        <option selected disabled>Elige tu Semestre</option>
+                                                        <option value="A">A</option>
+                                                        <option value="B">B</option>
+                                                        <option value="C">C</option>
+                                                        <option value="D">D</option>
+                                                    </select>
+                                                </div>
+                                                <!-- Jornada -->
+                                                <div class="pt-2">
+                                                    <label class="form-label fs-5" for="dayTrip_input">Jornada</label>
+                                                    <select class="form-select" name="dayTrip_input" id="dayTrip">
+                                                        <option selected disable>Elige tu Semestre</option>
+                                                        <option value="Vespertina">Vespertina</option>
+                                                        <option value="Nocturna">Nocturna</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
                         </div>
-                        <div class="col-6">
-                            <!-- Entidad -->
-                            <legend class="container">
-                                <label for="entity_input">Entidad</label>
-                                <select class="form-select" name="entity_input" id="entity">
-                                    <option selected disabled>Selecciona una Entidad</option>
-                                    <option value="Entidad 1">Entidad 1</option>
-                                    <option value="Entidad 2">Entidad 2</option>
-                                    <option value="Entidad 3">Entidad 3</option>
-                                    <option value="Entidad 4">Entidad 4</option>
-                                </select>
-                            </legend>
-                            <!-- Dirección de Domicilio -->
-                            <legend class="container">
-                                <label class="form-label" for="addressCity_input">Dirección</label>
-                                <input class="form-control" type="text" name="addressCity_input"
-                                    placeholder="Dirección Domiciliaria" required>
-                            </legend>
-                            <!-- Sector -->
-                            <legend class="container">
-                                <label class="form-label" for="quadrant_input">Sector</label>
-                                <input class="form-control" type="text" name="quadrant_input" placeholder="Sector"
-                                    required>
-                            </legend>
-                            <!-- Barrio -->
-                            <legend class="container">
-                                <label class="form-label" for="neighborhood_input">Barrio</label>
-                                <input class="form-control" type="text" name="neighborhood_input" placeholder="Barrio"
-                                    required>
-                            </legend>
-                            <!-- Semestre -->
-                            <legend class="container">
-                                <label class="form-label" for="semester_input">Semestre</label>
-                                <input class="form-control" type="text" name="semester_input" placeholder="Semestre"
-                                    required>
-                            </legend>
+                        <div class="container">
+                            <fieldset>
+                                <legend class="text-center">Sitio de Prácticas</legend>
+                                <div class="container ps-4 pe-4">
+                                    <!-- Entidad -->
+                                    <div class="container">
+                                        <label class="form-label fs-5" for="entity_input">Institución</label>
+                                        <select class="form-select" name="entity_input" id="entity">
+                                            <option selected disabled>Selecciona la Institución</option>
+                                            <option value="Entidad 1">Entidad 1</option>
+                                            <option value="Entidad 2">Entidad 2</option>
+                                            <option value="Entidad 3">Entidad 3</option>
+                                            <option value="Entidad 4">Entidad 4</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </fieldset>
                         </div>
                     </div>
                 </div>
@@ -147,7 +190,7 @@
     </main>
 
     <footer class="container-fluid d-flex align-items-center justify-content-center">
-        <span class="fs-5">Power By <a class="text-decoration-none" href="https://alfahost.es">AlfaHost</a></span>
+        <span class="fs-5">Power By <a class="text-decoration-none" href="#">YggdrasilCode</a></span>
     </footer>
 </body>
 
