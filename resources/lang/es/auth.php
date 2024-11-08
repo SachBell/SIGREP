3a17b5f404b61b6e1099c38a825ab11b1b71cfa4
SachBell <k_zack004@hotmail.com>
2024-11-08 10:20:06 -0500

    feat: :white_check_mark: Add a Translate Files

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+        'failed' => 'Usuario o contraseña erroneos.',
+        'throttle' => 'Límite de intentos superado. Intentalo más tarde.',
+        'password' => 'La contraseña proporcionada es incorrecta.',
+        'email' => 'El correo electrónico proporcionado no está registrado.',
+    ];
