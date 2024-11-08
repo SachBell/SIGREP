4b0573252f7f65a05a7b1d72a2534b49a39e90a6
SachBell <k_zack004@hotmail.com>
2024-11-08 10:14:35 -0500

    refactor: :truck: Update Routes with System

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [FormController::class, 'create'])->name('form.create');
+Route::post('/', [FormController::class, 'store'])->name('form.store');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
