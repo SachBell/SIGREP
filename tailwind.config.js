2491c54e3810ac4de38aa80ad9ded33f363dea80
SachBell <k_zack004@hotmail.com>
2024-11-24 18:52:56 -0500

    docs: :memo:

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/views/partials/*.blade.php'
+        './resources/views/partials/*.blade.php',
+        './node_modules/bootstrap/dist/js/**/*.js', // Asegura que Bootstrap se escanee
     ],
 
     theme: {
