05e9142c86b834021de5af031f56d69271b8fc9a
SachBell <132966392+SachBell@users.noreply.github.com>
2024-11-24 19:04:13 -0500

    feat: :wrench: Update Web Routes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ use App\Http\Controllers\FormController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
+Route::view('/form_registers', 'form_registers')->name('form_registers');
 
 Route::get('/', [FormController::class, 'create'])->name('form.create');
 Route::post('/', [FormController::class, 'store'])->name('form.store');
@@ -30,6 +29,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/registros', [FormController::class, 'index'])->name('registros.index');
+    Route::get('/registros/edit/{id}', [FormController::class, 'edit'])->name('registros.edit');
+    Route::put('/registros/{id}', [FormController::class, 'update'])->name('registros.update');
+    Route::delete('/registros/{id}', [FormController::class, 'destroy'])->name('registros.destroy');
+    Route::get('/registros/export', [FormController::class, 'export'])->name('registros.export');
 });
 
 require __DIR__.'/auth.php';
