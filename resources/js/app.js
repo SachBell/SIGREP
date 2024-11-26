aa827f319af6e11e918a9ccbc3d23ad51712d79d
SachBell <132966392+SachBell@users.noreply.github.com>
2024-11-25 20:25:53 -0500

    feat: :lipstick: New Function with button slidemenu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,11 @@
 import Alpine from 'alpinejs'
-window.Alpine = Alpine
-Alpine.start()
+document.addEventListener('DOMContentLoaded', () => {
+    // Obtener el botón y la barra lateral
+    const toggleBtn = document.getElementById('toggle-btn');
+    const sidebar = document.getElementById('sidebar');
+
+    toggleBtn.addEventListener('click', () => {
+        sidebar.classList.toggle('toggle');  // Alterna la clase d-none (ocultar) y d-block (mostrar)
+    });
+});
+
