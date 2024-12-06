e6e6f5b6928cd33d4398670224a23cf4c9549ac3
SachBell <132966392+SachBell@users.noreply.github.com>
2024-12-05 22:37:35 -0500

    refactor: :recycle: Refactor Routes with new Routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use App\Http\Controllers\FormController;
 */
 
 Route::view('/', 'welcome')->name('welcome');
-Route::view('/form_registers', 'form_registers')->name('form_registers');
 
 Route::get('/', [FormController::class, 'create'])->name('form.create');
 Route::post('/', [FormController::class, 'store'])->name('form.store');
@@ -29,11 +28,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/registros', [FormController::class, 'index'])->name('registros.index');
-    Route::get('/registros/edit/{id}', [FormController::class, 'edit'])->name('registros.edit');
-    Route::put('/registros/{id}', [FormController::class, 'update'])->name('registros.update');
-    Route::delete('/registros/{id}', [FormController::class, 'destroy'])->name('registros.destroy');
-    Route::get('/registros/export', [FormController::class, 'export'])->name('registros.export');
+    Route::get('/registros', [FormController::class, 'index'])->name('dashboard.registros.index');
+    Route::get('/registros/edit/{id}', [FormController::class, 'edit'])->name('dashboard.registros.edit');
+    Route::put('/registros/{id}', [FormController::class, 'update'])->name('dashboard.registros.update');
+    Route::delete('/registros/{id}', [FormController::class, 'destroy'])->name('dashboard.registros.destroy');
+    Route::get('/registros/export', [FormController::class, 'export'])->name('dashboard.registros.export');
 });
 
 require __DIR__.'/auth.php';
