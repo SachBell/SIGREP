c94b7bbc6436b1d4e7b1921131c9ca86a44c2d6b
SachBell <132966392+SachBell@users.noreply.github.com>
2024-12-05 22:36:36 -0500

    refactor: :recycle: Refactor Controller with New Models

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Formulario;
+use App\Models\UserData;
+use App\Models\User;
 use App\Models\Institucion;
+use App\Models\Grade;
+use App\Models\Semester;
 use App\Exports\FormularioExport;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -12,44 +16,42 @@ class FormController extends Controller
 {
 
     public function index(Request $request) {
-        
         $search = $request->input('search');
 
-        // Si se realiza una búsqueda y no hay resultados
+        // Consulta base para registros
+        $query = UserData::query();
+
+        // Si hay un término de búsqueda
         if (!empty($search)) {
-            $query = Formulario::query();
+            // Validar longitud mínima del término de búsqueda
+            if (strlen($search) < 3) {
+                return redirect()->back()->with('warning', 'Por favor, ingresa al menos 3 caracteres para realizar la búsqueda.');
+            }
+
+            // Filtrar registros por datos relacionados de `UserData`
             $query->where(function ($query) use ($search) {
                 $query->where('cei', 'like', '%' . $search . '%')
                     ->orWhere('name', 'like', '%' . $search . '%')
                     ->orWhere('lastname', 'like', '%' . $search . '%')
                     ->orWhere('email', 'like', '%' . $search . '%')
                     ->orWhere('phone_number', 'like', '%' . $search . '%');
-            }); 
+            });
 
-            // Verificar si no hay resultados
-            if ($query->exists()) {
-                // Si la búsqueda está vacía o tiene menos de 3 caracteres
-                if (!empty($search) && strlen($search) < 3) {
-                    return redirect()->back()->with('warning', 'Por favor, ingresa al menos 3 caracteres para realizar la búsqueda.');
-                }
-            } else {                
-                // Redirigir de vuelta sin mensajes
-                return redirect()->route('registros.index');
-            }
-
-            $registros = $query->with('institucion')->paginate(5);
-        } else {
-            // Si no hay búsqueda, obtener todos los registros
-            $registros = Formulario::with('institucion')->paginate(5);
         }
-        
-        return view('registers.index', compact('registros'));
+
+        // Cargar relaciones necesarias y paginar resultados
+        $registros = $query->with(['institutes', 'semesters', 'grades'])->paginate(5);
+
+        // dd($registros);
+        return view('dashboard.registers.index', compact('registros'));
     }
 
     public function create() {
         $entidades = Institucion::all();
-        $forms = Formulario::with('institucion')->get();
-        return view('welcome', compact('entidades', 'forms'));
+        $grades = Grade::all();
+        $semesters = Semester::all();
+
+        return view('welcome', compact('entidades', 'grades', 'semesters'));
     }
 
     public function store(Request $request) {
@@ -65,42 +67,48 @@ class FormController extends Controller
             'semester' => 'required',
             'grade' => 'required',
             'daytrip' => 'required',
-            'id_institucion' => 'required',
+            'id_institute' => 'required',
         ]);
 
-        $existente = Formulario::where('cei', $request->cei)
+        // dd($request);
+
+        $existente = UserData::where('cei', $request->cei)
             ->where('email', $request->email)
             ->first();
-
+        // dd($existente);
             if($existente) {
                 return redirect()->back()->with('error', 'Ya te has registrado en este formulario.');
             }
 
             // Crear y almacenar el formulario
 
-            Formulario::create($request->all());
+            UserData::create($request->all());
 
             return redirect('/')->with('success', 'Formulario enviado con éxito.');
     }
 
     public function destroy($id) {
 
-        $registro = Formulario::findOrFail($id);
+        $registro = UserData::findOrFail($id);
         $registro->delete();
 
-        return redirect()->route('registros.index')->with('success', 'Registro eliminado con éxito.');
+        return redirect()->route('dashboard.registros.index')->with('success', 'Registro eliminado con éxito.');
 
     }
 
     public function edit($id) {
-        $registro = Formulario::findOrFail($id);
-        $instituciones = Institucion::all();
-        return view('registers.partials.edit', compact('registro', 'instituciones'));
+        $registro = UserData::findOrFail($id);
+        $entidades = Institucion::all();
+        $semesters = Semester::all();
+        $grades = Grade::all();
+        return view('dashboard.registers.partials.edit', compact('registro', 'entidades', 'grades', 'semesters'));
     }
 
     public function update(Request $request, $id) {
         
-        $registro = Formulario::findOrFail($id);
+        $registro = UserData::findOrFail($id);
+
+        // dd($request->all());
 
         $request->validate([
             'cei' => 'required|numeric|digits_between:1,10',
@@ -110,15 +118,15 @@ class FormController extends Controller
             'email' => 'required|email',
             'address' => 'required',
             'neighborhood' => 'required',
-            'semester' => 'required',
-            'grade' => 'required',
+            'id_semester' => 'required',
+            'id_grade' => 'required',
             'daytrip' => 'required',
-            'id_institucion' => 'required',
+            'id_institute' => 'required',
         ]);
 
         $registro->update($request->all());
 
-        return redirect()->route('registros.index')->with('success', 'Registro Actualizado con Exito');
+        return redirect()->route('dashboard.registros.index')->with('success', 'Registro Actualizado con Exito');
     }
 
     public function export(){
