2c9c1a256aedd01ac9912dfb3f9eb5d4e7657f67
SachBell <132966392+SachBell@users.noreply.github.com>
2025-01-13 16:46:08 -0500

    feat: :lock: Added more security for administrator users

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -35,6 +36,13 @@ class NewPasswordController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $prohibitedRoles = explode(',', env('RESERVED_ROLES', ''));
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && in_array(strtolower($user->role_name), array_map('strtolower', $prohibitedRoles))) {
+            return redirect('/login')->with('error', 'No se puede recuperar esta cuenta.');
+        }   
+
         // Here we will attempt to reset the user's password. If it is successful we
         // will update the password on an actual user model and persist it to the
         // database. Otherwise we will parse the error and return the response.
@@ -54,8 +62,8 @@ class NewPasswordController extends Controller
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
-                    ? redirect()->route('login')->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withInput($request->only('email'))
+            ->withErrors(['email' => __($status)]);
     }
 }
--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
@@ -29,6 +30,13 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
+        $prohibitedRoles = explode(',', env('RESERVED_ROLES', ''));
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && in_array(strtolower($user->role_name), array_map('strtolower', $prohibitedRoles))) {
+            return redirect('/login')->with('error', 'No se puede recuperar esta cuenta.');
+        }
+
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
@@ -37,8 +45,8 @@ class PasswordResetLinkController extends Controller
         );
 
         return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+            ? back()->with('status', __($status))
+            : back()->withInput($request->only('email'))
+            ->withErrors(['email' => __($status)]);
     }
 }
