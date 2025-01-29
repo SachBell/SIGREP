0737eded6ea1b8c732926ffe5025e3ca26b69f15
SachBell <k_zack004@hotmail.com>
2025-01-29 12:36:09 -0500

    docs: :wrench: Update Translation Files

--- resources/lang/es/emails.php
+++ resources/lang/es/emails.php
@@ -1,9 +1,27 @@
 <?php
 
 return [
-    'reset_greeting_title' => 'Hola, :name',
-    'reset_password_subject' => 'Restablecimiento de contraseña',
-    'reset_password_line' => 'Haz clic en el siguiente enlace para restablecer tu contraseña:',
-    'reset_password_button' => 'Restablecer contraseña',
-    'reset_password_footer' => 'Si no solicitaste este restablecimiento, ignora este mensaje.',
+    'reset' => [
+        'greeting_title' => 'Hola, :name',
+        'password_subject' => 'Restablecimiento de contraseña',
+        'password_firts_line' => 'Hemos recibido una solicitud para cambiar tu contraseña.',
+        'password_second_line' => 'Haz clic en el siguiente botón para restablecer tu contraseña y asegurar el acceso a tu cuenta:',
+        'password_button' => 'Restablecer contraseña',
+        'password_footer' => 'Si no solicitaste este restablecimiento de contraseña, por favor ignora este mensaje. Si no fuiste tú, te recomendamos cambiar tu contraseña de inmediato.',
+        'email_footer' => '© :year :app_name - Todos los derechos reservados.'
+    ],
+
+    'verify' => [
+        'greeting_title' => '¡Hola, :name!',
+        'verify_subject' => 'Verificación de Correo Electrónico',
+        'verify_line' => 'Hemos recibido una solicitud para verificar tu dirección de correo electrónico',
+        'verify_button' => 'Verificar mi correo',
+        'verify_footer' => 'Si no realizaste esta solicitud, simplemente ignora este mensaje',
+        'email_footer' => '© :year :app_name - Todos los derechos reservados.'
+    ],
+
+    'regards' => [
+        'regards' => 'Saludos',
+        'support' => 'Soporte ',
+    ]
 ];
