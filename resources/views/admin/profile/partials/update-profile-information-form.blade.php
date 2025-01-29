b1d5eec0e9b95ed64078cc04a1209dea108eb1f5
SachBell <k_zack004@hotmail.com>
2025-01-28 22:28:47 -0500

    style: :lipstick: Update UI Styles Admin Dashboar

--- resources/views/admin/profile/partials/update-profile-information-form.blade.php
+++ resources/views/admin/profile/partials/update-profile-information-form.blade.php
@@ -33,11 +33,11 @@
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
-                        {{ __('Your email address is unverified.') }}
+                        {{ __('Su correo electrónico no está verificado') }}
 
                         <button form="send-verification"
                             class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Click here to re-send the verification email.') }}
+                            {{ __('Haga click aquí para enviar la verificación.') }}
                         </button>
                     </p>
 
