5c46d2a19c466fc19b27215ef354c484211e84d6
SachBell <k_zack004@hotmail.com>
2025-01-28 21:07:23 -0500

    docs: :wrench: Add new Translations Files

---
+++ resources/lang/es/emails.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'reset_password_subject' => 'Restablecimiento de contraseña',
+    'reset_password_line' => 'Haz clic en el siguiente enlace para restablecer tu contraseña:',
+    'reset_password_button' => 'Restablecer contraseña',
+    'reset_password_footer' => 'Si no solicitaste este restablecimiento, ignora este mensaje.',
+];
