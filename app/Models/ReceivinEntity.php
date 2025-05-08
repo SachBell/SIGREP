3ffa6b434fe32d646f33d70fdb5899c690b530f5
SachBell <k_zack004@hotmail.com>
2025-05-08 01:20:30 -0500

    refactor: :truck: Renamed Institutes Model by ReceivingEntity Model

--- app/Models/ReceivinEntity.php
+++ app/Models/ReceivinEntity.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
-class Institute extends Model
+class ReceivinEntity extends Model
 {
     use HasFactory, Searchable;
 
