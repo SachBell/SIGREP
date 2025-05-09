f44a1c6b0ada8bf5c0fea0852f6fa381a36503f8
SachBell <k_zack004@hotmail.com>
2025-05-09 02:03:12 -0500

    refactor: :wrench: Updated Role Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,18 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role as SpatieRole;
 
-class Role extends Model
+class Role extends SpatieRole
 {
     use HasFactory;
 
     protected $table = 'roles';
 
     protected $fillable = [
-        'role_name'
+        'name',
+        'guard_name'
     ];
-
-    public function users() {
-        return $this->hasMany(User::class, 'id_role');
-    }
 }
